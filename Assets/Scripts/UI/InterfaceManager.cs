e5362a9675fb5d90d89e706a8ff5a855356e0a5d
Erich Querner <erichquerner@posteo.org>
2024-06-28 23:02:50 +0200

    add textmesh pro

---
+++ Assets/Scripts/UI/InterfaceManager.cs
@@ -0,0 +1,35 @@
+using System;
+using TuioSimulation.Input;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace TuioSimulator.UI
+{
+    public class InterfaceManager : MonoBehaviour
+    {
+        [SerializeField] private MouseClicker _background;
+        [SerializeField] private SimulatorMenu _defaultMenu;
+
+        private void OnEnable()
+        {
+            _background.OnLeftClick += CloseDefaultMenu;
+            _background.OnRightClick += OpenDefaultMenu;
+        }
+
+        private void OnDisable()
+        {
+            _background.OnLeftClick -= CloseDefaultMenu;
+            _background.OnRightClick -= OpenDefaultMenu;
+        }
+
+        private void CloseDefaultMenu(Vector2 position)
+        {
+            _defaultMenu.Close();
+        }
+
+        private void OpenDefaultMenu(Vector2 position)
+        {
+            _defaultMenu.Open(position);
+        }
+    }
+}
