c3c70662d965f3b0d68928fe64cbf2b04ba65ebe
Erich Querner <querner@interactive-scape.com>
2024-11-21 10:46:43 +0100

    workaround to set id of next object

---
+++ Assets/Scripts/Tuio/Common/CurrentIdSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TuioSimulator.Tuio.Common
+{
+    [CreateAssetMenu(fileName = "CurrentId", menuName = "TuioSimulator/New CurrentID", order = 0)]
+    public class CurrentIdSO : ScriptableObject
+    {
+        [field:SerializeField] public uint CurrentId { get; set; }
+    }
+}
---
+++ Assets/Scripts/UI/IdSetter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TMPro;
+using TuioSimulator.Tuio.Common;
+using UnityEngine;
+
+namespace TuioSimulator.UI
+{
+    public class IdSetter : MonoBehaviour
+    {
+        [SerializeField] private CurrentIdSO _currentId;
+        [SerializeField] private TMP_InputField _inputField;
+
+        private void Start()
+        {
+            _inputField.text = _currentId.CurrentId.ToString();
+        }
+
+        private void OnEnable()
+        {
+            _inputField.onValueChanged.AddListener(UpdateCurrentId);
+        }
+
+        private void OnDisable()
+        {
+            _inputField.onValueChanged.RemoveAllListeners();
+        }
+
+        private void UpdateCurrentId(string inputValue)
+        {
+            uint id = uint.TryParse(inputValue, out var newId) ? newId : 0;
+            _currentId.CurrentId = id;
+        }
+    }
+}
