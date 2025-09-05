0a8938c28fcc54faef2e63133e1f1cba6e724eae
Erich Querner <querner@interactive-scape.com>
2025-09-05 14:28:05 +0200

    more precise rotation with shift key

--- Assets/Scripts/Input/MouseScroller.cs
+++ Assets/Scripts/Input/MouseScroller.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace TuioSimulator.Input
 {
@@ -15,7 +16,8 @@ namespace TuioSimulator.Input
         public void OnScroll(PointerEventData eventData)
         {
             var scrollDelta = eventData.scrollDelta.y;
-            _rectTransform.Rotate(Vector3.back, scrollDelta);
+            var modifier = Keyboard.current.leftShiftKey.isPressed ? 0.1f : 1f;
+            _rectTransform.Rotate(Vector3.forward, scrollDelta * modifier);
         }
     }
 }
