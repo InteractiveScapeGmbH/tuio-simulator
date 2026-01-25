bae5e784f5bd2ff6359145a23966e697eb673dc0
Erich Querner <erichquerner@posteo.org>
2026-01-25 11:33:24 +0100

    make fields private since they are only used in the class itself

--- Assets/Scripts/Tuio/Common/TuioTransform.cs
+++ Assets/Scripts/Tuio/Common/TuioTransform.cs
@@ -7,26 +7,26 @@ namespace TuioSimulator.Tuio.Common
     public abstract class TuioTransform : DebugTuio
     {
         protected RectTransform RectTransform;
-        protected Vector2 LastPosition;
+        private Vector2 _lastPosition;
         protected float LastAngle;
         protected float Angle => (-RectTransform.localEulerAngles.z + 360f) * Mathf.Deg2Rad;
         protected Vector2 NormalizedPosition;
 
-        protected RectTransform Parent;
-        protected Vector2 _Position;
+        private RectTransform _parent;
+        private Vector2 _position;
 
         protected TuioTime Time;
 
         public Vector2 Position
         {
-            get => _Position;
+            get => _position;
             set
             {
-                if(RectTransformUtility.ScreenPointToLocalPointInRectangle(Parent, value, Camera.main, out var localPoint))
+                if(RectTransformUtility.ScreenPointToLocalPointInRectangle(_parent, value, Camera.main, out var localPoint))
                 {
-                    _Position = localPoint;
+                    _position = localPoint;
                     RectTransform.anchoredPosition = localPoint;
-                    var normalizedPosition = Rect.PointToNormalized(Parent.rect, localPoint);
+                    var normalizedPosition = Rect.PointToNormalized(_parent.rect, localPoint);
                     normalizedPosition.y = 1.0f - normalizedPosition.y;
                     NormalizedPosition = normalizedPosition;
                 }
@@ -38,17 +38,17 @@ namespace TuioSimulator.Tuio.Common
         protected void Awake()
         {
             RectTransform = GetComponent<RectTransform>();
-            Parent = transform.parent as RectTransform;
+            _parent = transform.parent as RectTransform;
             Time = TuioTime.GetSystemTime();
         }
 
         protected void Update()
         {
             Time = TuioTime.GetSystemTime();
-            var velocity = NormalizedPosition - LastPosition;
+            var velocity = NormalizedPosition - _lastPosition;
             var rotationSpeed = Angle - LastAngle;
             UpdateTuio(velocity, rotationSpeed);
-            LastPosition = NormalizedPosition;
+            _lastPosition = NormalizedPosition;
             LastAngle = Angle;
         }
     }
